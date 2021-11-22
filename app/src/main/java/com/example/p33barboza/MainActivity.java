33b306e8e8d83d2ed380612b310ee0274337ae10
brais barboza <brais.barboza@udc.es>
2021-11-22 16:39:13 +0100

    Practica terminada

--- app/src/main/java/com/example/p33barboza/MainActivity.java
+++ app/src/main/java/com/example/p33barboza/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.p33barboza;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 import androidx.preference.PreferenceManager;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -23,13 +28,14 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private static final String KEY_USER = "KEY_USER";
     Date date;
 
     String filename = "usernames.txt";
-    Button button;
+    Button button, send;
     EditText et;
     TextView tv;
     SharedPreferences sharedPreferences;
@@ -63,9 +69,33 @@ public class MainActivity extends AppCompatActivity {
 
     private void setUI() {
         button = findViewById(R.id.button);
+        send = findViewById(R.id.send_button);
         et = findViewById(R.id.et);
         tv = findViewById(R.id.tv);
         File file = new File(getFilesDir(), filename);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                File newFile = new File(getFilesDir(), "usernames.txt");
+                Uri contentUri = FileProvider.getUriForFile(getApplicationContext(),
+                        BuildConfig.APPLICATION_ID + ".fileprovider",
+                        newFile);
+
+                Intent intentShareFile = new Intent(Intent.ACTION_SEND)
+                        .putExtra( Intent.EXTRA_STREAM, contentUri)
+                        .setType( "text/*");
+                Intent chooser = Intent.createChooser(intentShareFile, "Share File");
+
+                List<ResolveInfo> resInfoList = getPackageManager().queryIntentActivities(chooser, PackageManager.MATCH_DEFAULT_ONLY);
+
+                for (ResolveInfo resolveInfo : resInfoList) {
+                    String packageName = resolveInfo.activityInfo.packageName;
+                    grantUriPermission(packageName, contentUri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                }
+
+                startActivity(chooser);
+            }
+        });
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -78,7 +108,7 @@ public class MainActivity extends AppCompatActivity {
                 editor.putString(KEY_USER, tmp);
                 editor.apply();
                 try (FileOutputStream fos = openFileOutput(filename, MODE_PRIVATE | MODE_APPEND)) {
-                    String fileContents = "User: " + sharedPreferences.getString(KEY_USER, getResources().getString(R.string.tv_default)) + ", Date: " + strDate+ "\n";
+                    String fileContents = "User: " + sharedPreferences.getString(KEY_USER, getResources().getString(R.string.tv_default)) + ", Date: " + strDate + "\n";
                     fos.write(fileContents.getBytes(StandardCharsets.UTF_8));
                 } catch (IOException e) {
 
